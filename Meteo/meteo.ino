f0cbc852d49ea290a8b8d35392e9f1f21d7a1644
PuFFeG <puffeg@gmail.com>
2014-04-09 17:05:37 +0300

    Meteo
    Hell meteo for farm

---
+++ Meteo/meteo.ino
@@ -0,0 +1,131 @@
+// ========================Задаем необходимые библиотеки==========
+#include <SPI.h>
+#include <Ethernet.h>
+#include <DHT.h>
+// ===============================================================
+// ========================Задаем данные сети======================
+byte mac[] = { 0xCA, 0xAF, 0x78, 0x1C, 0x13, 0x77 }; //mac - адрес ethernet shielda
+byte ip[] = { 192, 168, 1, 33 };    	// ip адрес ethernet shielda
+byte subnet[] = { 255, 255, 255, 0 }; //маска подсети
+EthernetServer server(80); //порт сервера
+int ledPin = 4; // указываем что светодиод будет управляться через 4 Pin
+int temp2 = 5; //Порт лм35 5
+String readString = String(30); //string for fetching data from address
+boolean LEDON = false; //изначальный статус светодиода - выключен
+// ===============================================================
+#define DHTPIN 3	// пин для датчика DHT22
+const unsigned char OSS = 0;  // Oversampling Setting
+// ===============================================================
+
+// ==============================================================
+DHT dht(DHTPIN, DHT11);
+// ========================СТАРТУЕМ=============================
+// ========================Управляем св.диодом на 4-м пине==========
+void setup(){
+  //запускаем Ethernet
+  Ethernet.begin(mac, ip, subnet);
+  //устанавливаем pin 4 на выход
+  pinMode(ledPin, OUTPUT);
+  //enable serial datada print
+  Serial.begin(9600);
+  	Serial.println("Port Test!"); // Тестовые строки для отображения в мониторе порта
+  	Serial.println("GO!");// Тестовые строки для отображения в мониторе порта
+  dht.begin();
+}
+// ==============================================================
+void loop(){
+  // ========================ДАТЧИКИ=============================
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float tempC = analogRead(temp2); //read the value from the sensor
+  tempC = tempC/9,31; //convert the analog data to temperature
+  if (isnan(t) || isnan(h)) {
+	Serial.println("Failed to read from DHT");
+  } else {
+  	Serial.print("H=: ");
+	Serial.print(h);
+  	Serial.print(" %\t");
+  	Serial.print("Temp.=: ");
+	Serial.print(t);
+  	Serial.println(" *C");
+    	Serial.println("Temp2 = ");
+  	Serial.print(tempC);
+ // ==============================================================
+   // =============Создаем клиентское соединение====================
+  EthernetClient client = server.available();
+  if (client) {
+	while (client.connected()) {
+	if (client.available()) {
+  	char c = client.read();
+  	//read char by char HTTP request
+  	if (readString.length() < 30) {
+    	//store characters to string
+    	readString.concat( c); }
+    	//output chars to serial port
+    	Serial.print( c);
+    	//if HTTP request has ended
+    	if (c == '\n') {
+    	//Проверяем включили ли светодиод?
+    	//Level=1 - включен
+    	//Level=0 - выключен
+      	if(readString.indexOf("Level=1") >=0) {
+        	//Включаем светодиод
+        	digitalWrite(ledPin, HIGH); // set the LED on
+        	LEDON = true;
+      	}else{
+        	//Выключаем светодиод
+        	digitalWrite(ledPin, LOW); // set the LED OFF
+        	LEDON = false;
+      	}	 
+   // =============Формируем HTML-страницу==========================
+      	client.println("HTTP/1.1 200 OK");
+      	client.println("Content-Type: text/html");
+      	client.println();
+      	client.println("<head> ");
+      	client.println("<meta http-equiv='Content-Type' content='text/html; charset=utf-8' /> ");
+      	client.println("<title> :: Погода в ШАХТЕ :: </title>");
+      	client.println("</head> ");
+      	client.println("<body");
+      	client.println("<hr />");
+      	client.println("<h1> ::HELL METEO:: </h1>");
+      	if (LEDON){
+        	client.println("<form method=get name=LED><input type=radio name=Level value=1 CHECKED>Включить<input type=radio name=Level value=0>Выключить<input type=submit value=OK></form>");
+        	client.println("<font size=’5′>Satan-Lamp: ");
+        	client.println("<font size=’5′>On.");
+      	}else{
+        	client.println("<form method=get name=LED><input type=radio name=Level value=1>Включить<input type=radio name=Level value=0 CHECKED>Выключить<input type=submit value=OK></form>");
+        	client.println("<font size=’5′>Satan-Lamp: ");
+        	client.println("<font size=’5′>Off");
+      	}
+     	//==============Вывод значений на web-страницу======================
+          	client.println("<hr />");//линия=====================================
+        	client.println("Tемпература = ");	//Температура с DHT 22
+        	client.println(t);
+        	client.println(" *C");
+          	client.println("<br> "); //перенос на след. строчку
+        	client.println("Влажность = ");	//Влажность с DHT 22
+        	client.println(h);
+        	client.println(" %\t");
+          	client.println("<br> "); //перенос на след. строчку
+              	client.println("HELLx = ");	//Температура 2
+        	client.println(tempC);
+               	client.println(" *C");
+          	client.println("<br> "); //перенос на след. строчку
+          	client.println("<hr />");//линия=====================================
+        	//==============================================================
+        	client.println("</body></html>");
+        	//очищаем строку для следующего считывания 	 
+        	//==============Останавливаем web-client===========================
+        	readString="";
+        	client.stop();
+        	//==============================================================
+      	}
+    	}
+  	}
+ 	}
+ }
+ }
+
+
+
+
