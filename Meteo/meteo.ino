64b7e505ef6d240064e73c62e7d4a80ddc47c0a8
PuFFeG <puffeg@gmail.com>
2014-05-05 15:37:25 +0300

    Update meteo.ino
    3 dht

--- Meteo/meteo.ino
+++ Meteo/meteo.ino
@@ -8,37 +8,46 @@ byte mac[] = { 0xCA, 0xAF, 0x78, 0x1C, 0x13, 0x77 }; //mac - адрес ethernet
 byte ip[] = { 192, 168, 1, 33 };    	// ip адрес ethernet shielda
 byte subnet[] = { 255, 255, 255, 0 }; //маска подсети
 EthernetServer server(80); //порт сервера
-int ledPin = 4; // указываем что светодиод будет управляться через 4 Pin
-int temp2 = 5; //Порт лм35 5
+int ledPin = 6; // указываем что светодиод будет управляться через 4 Pin
 String readString = String(30); //string for fetching data from address
 boolean LEDON = false; //изначальный статус светодиода - выключен
 // ===============================================================
-#define DHTPIN 3	// пин для датчика DHT22
+#define DHTPIN 2	// пин для датчика DHT22
+#define DHTPIN1 3	// пин для датчика DHT22
+#define DHTPIN2 4	// пин для датчика DHT22
 const unsigned char OSS = 0;  // Oversampling Setting
 // ===============================================================
 
 // ==============================================================
 DHT dht(DHTPIN, DHT11);
+DHT dht1(DHTPIN1, DHT11);
+DHT dht2(DHTPIN2, DHT11);
 // ========================СТАРТУЕМ=============================
 // ========================Управляем св.диодом на 4-м пине==========
 void setup(){
   //запускаем Ethernet
   Ethernet.begin(mac, ip, subnet);
-  //устанавливаем pin 4 на выход
+  //устанавливаем pin 6 на выход
   pinMode(ledPin, OUTPUT);
   //enable serial datada print
   Serial.begin(9600);
   	Serial.println("Port Test!"); // Тестовые строки для отображения в мониторе порта
   	Serial.println("GO!");// Тестовые строки для отображения в мониторе порта
   dht.begin();
+  dht1.begin();
+  dht2.begin();
+
 }
 // ==============================================================
 void loop(){
   // ========================ДАТЧИКИ=============================
   float h = dht.readHumidity();
   float t = dht.readTemperature();
-  float tempC = analogRead(temp2); //read the value from the sensor
-  tempC = tempC/9,31; //convert the analog data to temperature
+  float h1 = dht1.readHumidity();
+  float t1 = dht1.readTemperature();
+  float h2 = dht2.readHumidity();
+  float t2 = dht2.readTemperature();
+
   if (isnan(t) || isnan(h)) {
 	Serial.println("Failed to read from DHT");
   } else {
@@ -48,8 +57,20 @@ void loop(){
   	Serial.print("Temp.=: ");
 	Serial.print(t);
   	Serial.println(" *C");
-    	Serial.println("Temp2 = ");
-  	Serial.print(tempC);
+    	Serial.println("Temp2 ======= ");
+      	Serial.print("H2=: ");
+	Serial.print(h1);
+  	Serial.print(" %\t");
+  	Serial.print("Temp2.=: ");
+	Serial.print(t1);
+  	Serial.println(" *C");
+    	Serial.println("Temp3 =\\\\\\\\\\ ");
+      	Serial.print("H3=: ");
+	Serial.print(h2);
+  	Serial.print(" %\t");
+  	Serial.print("Temp3.=: ");
+	Serial.print(t2);
+  	Serial.println(" *C");
  // ==============================================================
    // =============Создаем клиентское соединение====================
   EthernetClient client = server.available();
@@ -107,10 +128,23 @@ void loop(){
         	client.println(h);
         	client.println(" %\t");
           	client.println("<br> "); //перенос на след. строчку
-              	client.println("HELLx = ");	//Температура 2
-        	client.println(tempC);
+              	client.println("Tемпература = ");	//Температура 2
+        	client.println(t1);
                	client.println(" *C");
           	client.println("<br> "); //перенос на след. строчку
+        	client.println("Влажность = ");	//Влажность с DHT 22
+        	client.println(h1);
+        	client.println(" %\t");
+          	client.println("<br> "); //перенос на след. строчку
+                client.println("Tемпература = ");	//Температура 2
+        	client.println(t2);
+               	client.println(" *C");
+          	client.println("<br> "); //перенос на след. строчку
+        	client.println("Влажность = ");	//Влажность с DHT 22
+        	client.println(h2);
+        	client.println(" %\t");
+          	client.println("<br> "); //перенос на след. строчку
+
           	client.println("<hr />");//линия=====================================
         	//==============================================================
         	client.println("</body></html>");
@@ -125,7 +159,3 @@ void loop(){
  	}
  }
  }
-
-
-
-
